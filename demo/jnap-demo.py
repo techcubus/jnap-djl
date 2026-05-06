e1b16c1dd3310d3b5a2d219314122de4ddaf2d6a
Lily the Elder <djlilis@yahoo.com>
2026-05-05 20:18:12 -0500

    Labeled sections in demo output

--- demo/jnap-demo.py
+++ demo/jnap-demo.py
@@ -15,26 +15,27 @@ PING_TARGET = "192.168.1.200"
 PING_COUNT = 5
 PING_TIME_OUT = 60
 PING_SLEEP_FOR = 2
+TRACERT_TARGET = "google.com"
 
 addr = sys.argv[1]
 router = Linksys(addr)
 
 pw = getpass.getpass("Enter admin password for '"+str(addr)+"': ")
 router.password(pw)
-print (json.dumps(router.check_password(pw).json(), indent=2))
+print ("check_password = " + json.dumps(router.check_password(pw).json(), indent=2))
 
 # Checking whether router has default password
-print (json.dumps(router.has_default_password().json(), indent=2))
+print ("has_default_password = " + json.dumps(router.has_default_password().json(), indent=2))
 
 # Listing all users
-print (json.dumps(router.get_users().json(), indent=2))
+print ("get_users = " + json.dumps(router.get_users().json(), indent=2))
 
 # Getting a bunch of device info
-print (json.dumps(router.get_device_info().json(), indent=2))
+print ("get_deice_info = " + json.dumps(router.get_device_info().json(), indent=2))
 
 # Testing out the ping functionality
-print (json.dumps(router.stop_ping().json(), indent=2))
-print (json.dumps(router.start_ping(host=PING_TARGET,count=PING_COUNT).json(), indent=2))
+print ("stop_ping = " + json.dumps(router.stop_ping().json(), indent=2))
+print ("start_ping = " + json.dumps(router.start_ping(host=PING_TARGET,count=PING_COUNT).json(), indent=2))
 
 # Check every 2 seconds to see if ping is done. TIMEOUT after 1 minute
 TIMEOUT=PING_TIME_OUT
@@ -47,12 +48,12 @@ while TIMEOUT > 0:
     TIMEOUT -= PING_SLEEP_FOR
     sleep(PING_SLEEP_FOR)
 
-print (json.dumps(router.get_ping_status().json(), indent=2))
-print (json.dumps(router.stop_ping().json(), indent=2))
+print ("get_ping_status = " + json.dumps(router.get_ping_status().json(), indent=2))
+print ("stop_ping = " + json.dumps(router.stop_ping().json(), indent=2))
 
 # Test out the traceroute functionality from the router to google.com
-print (json.dumps(router.stop_traceroute().json(), indent=2))
-print (json.dumps(router.start_traceroute("google.com").json(), indent=2))
+print ("stop_traceroute = " + json.dumps(router.stop_traceroute().json(), indent=2))
+print ("start_traceroute = " + json.dumps(router.start_traceroute(TRACERT_TARGET).json(), indent=2))
 
 # Reset the clock
 TIMEOUT = PING_TIME_OUT
@@ -64,8 +65,8 @@ while TIMEOUT > 0:
     TIMEOUT -= PING_SLEEP_FOR
     sleep(PING_SLEEP_FOR)
 
-print (json.dumps(router.stop_traceroute().json(), indent=2))
-print (json.dumps(router.get_traceroute_status().json(), indent=2))
+print ("stop_traceroute = " + json.dumps(router.stop_traceroute().json(), indent=2))
+print ("get_traceroute_status = " + json.dumps(router.get_traceroute_status().json(), indent=2))
 
 
 
